3982aa1530c939c652f657af78d0a079f703210b
TaNu <134486612+TaNuNian@users.noreply.github.com>
2024-06-03 15:54:25 +0700

    fix build.gradle and settings.gradle

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -7,7 +7,6 @@ import 'decor/theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
