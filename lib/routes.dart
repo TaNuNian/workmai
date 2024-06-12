8e8cf9311eab8993e20839b573eec2e8565ff0b9
OR_TH <watcharaw.boon@gmail.com>
2024-06-12 20:30:36 +0700

    rename create_acc_skill.dart 2

--- lib/routes.dart
+++ lib/routes.dart
@@ -2,8 +2,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:workmai/src/pre_pages/create_account_pages/create_acc_intro/create_acc_intro.dart';
 import 'package:workmai/src/pre_pages/create_account_pages/create_acc_ness/create_acc_ness.dart';
-import 'package:workmai/src/pre_pages/create_account_pages/create_acc_ness/create_acc_select_skill/create_acc_skill.dart';
 import 'package:workmai/src/pre_pages/create_account_pages/create_acc_intro/create_acc_unness_intro.dart';
+import 'package:workmai/src/pre_pages/create_account_pages/create_acc_ness/create_acc_skill/create_acc_skill.dart';
 import 'package:workmai/src/pre_pages/login_page/login_page_2.dart';
 import 'package:workmai/src/pre_pages/register_page/register_page.dart';
 
