fd415b6a91bae3069ffcfb6f44407b9065935110
OR_TH <watcharaw.boon@gmail.com>
2024-06-19 20:08:20 +0700

    fix conflict my_profile.dart

--- lib/src/main_pages/profile_pages/my_profile.dart
+++ lib/src/main_pages/profile_pages/my_profile.dart
@@ -1,31 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:workmai/src/decor/padding.dart';
-import 'package:workmai/src/main_pages/profile_pages/my_profile_wg/inter_tag.dart';
-import 'package:workmai/src/main_pages/profile_pages/my_profile_wg/myprofile_appear.dart';
-
-import 'package:workmai/src/decor/divider.dart';
-import 'package:workmai/src/main_pages/profile_pages/my_profile_wg/skill_tag.dart';
+import 'package:workmai/src/main_pages/profile_pages/profile_page.dart';
+import 'package:workmai/src/main_pages/profile_pages/profile_wg/myprofile_appear_edit.dart';
 
 class MyProfile extends StatelessWidget {
   const MyProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              MyProfileAppear(),
-              CustomDivider(),
-              InterTag(),
-              SkillTag(),
-              CustomDivider(),
-              // AboutMe(),
-            ],
-          ),
-        )
-      ),
+    return const Center(
+      child: ProfilePage(button: MyprofileAppearEdit(),),
     );
   }
 }
