6ebb5108b05b9bee2ca77d5b3d353ef22f77084a
OR_TH <watcharaw.boon@gmail.com>
2024-06-29 13:12:41 +0700

    resolve conflict 1

--- lib/src/main_pages/profile_pages/profile_wg/myprofile_appear_name.dart
+++ lib/src/main_pages/profile_pages/profile_wg/myprofile_appear_name.dart
@@ -6,8 +6,7 @@ import 'package:workmai/methods/user_provider.dart';
 class MyprofileAppearName extends StatefulWidget {
   final String username;
 
-  const MyprofileAppearName(
-      {super.key, required this.username});
+  const MyprofileAppearName({super.key, required this.username});
 
   @override
   State<MyprofileAppearName> createState() => _MyprofileAppearNameState();
@@ -51,62 +50,67 @@ class _MyprofileAppearNameState extends State<MyprofileAppearName> {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       mainAxisAlignment: MainAxisAlignment.end,
-      children: [Consumer<UserProvider>(
-        builder: (context, userProvider, child){
-          if (userProvider.userData == null) {
-            return Center(child: CircularProgressIndicator());
-          }
-          final displayName = userProvider.userData!["profile"]["display_name"];
-          final userName = userProvider.userData!["profile"]["name"];
-          return Padding(
-
-            padding: const EdgeInsets.only(left: 8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  displayName != null && displayName.isNotEmpty ? displayName : 'Display name',
-                  style: GoogleFonts.sarabun(
-                    color: const Color(0xff327B90),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 32,
-                  ),
-                ),
-                Row(
+      children: [
+        Consumer<UserProvider>(
+          builder: (context, userProvider, child) {
+            if (userProvider.userData == null) {
+              return const Center(child: Text('ERROR', style: TextStyle(color: Colors.red, fontWeight: FontWeight.w900, fontSize: 20),));
+            }
+            else {
+              final displayName =
+                  userProvider.userData!["profile"]["display_name"];
+              final userName = userProvider.userData!["profile"]["name"];
+              return Padding(
+                padding: const EdgeInsets.only(left: 8),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
-                      '@'+userName,
+                      displayName != null && displayName.isNotEmpty
+                          ? displayName
+                          : 'Display name',
                       style: GoogleFonts.sarabun(
-                        color: const Color(0xff59A1B6),
-                        fontWeight: FontWeight.w300,
-                        fontSize: 22,
+                        color: const Color(0xff327B90),
+                        fontWeight: FontWeight.bold,
+                        fontSize: 32,
                       ),
                     ),
-                    const SizedBox(width: 20),
-                    Container(
-                      width: 160,
-                      height: 35,
-                      decoration: BoxDecoration(
-                        color: const Color(0xffc3e1ea),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: const Padding(
-                        padding: EdgeInsets.symmetric(horizontal: 8),
-                        child: Row(
-                          children: [
-                            Icon(Icons.military_tech),
-                            Icon(Icons.star),
-                          ],
+                    Row(
+                      children: [
+                        Text(
+                          '@' + userName,
+                          style: GoogleFonts.sarabun(
+                            color: const Color(0xff59A1B6),
+                            fontWeight: FontWeight.w300,
+                            fontSize: 22,
+                          ),
                         ),
-                      ),
+                        const SizedBox(width: 20),
+                        Container(
+                          width: 160,
+                          height: 35,
+                          decoration: BoxDecoration(
+                            color: const Color(0xffc3e1ea),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 8),
+                            child: Row(
+                              children: [
+                                Icon(Icons.military_tech),
+                                Icon(Icons.star),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
                     )
                   ],
-                )
-              ],
-            ),
-          );
-        },
-      ),
+                ),
+              );
+            }
+          },
+        ),
       ],
     );
   }
