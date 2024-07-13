a8e90e4905298142be2239be6bf90e0f5fe63fcc
OR_TH <watcharaw.boon@gmail.com>
2024-07-13 19:04:32 +0700

    new news_page.dart

--- lib/src/main_pages/news_page/news_page.dart
+++ lib/src/main_pages/news_page/news_page.dart
@@ -4,129 +4,168 @@ import 'package:workmai/src/decor/search_tab.dart';
 import 'package:workmai/src/decor/textfield_decor.dart';
 import 'package:workmai/src/main_pages/news_page/news_tile.dart';
 
-class NewsPage extends StatefulWidget {
+class NewsPage extends StatelessWidget {
   const NewsPage({super.key});
 
   @override
-  State<NewsPage> createState() => _NewsPageState();
-}
-
-class _NewsPageState extends State<NewsPage> {
-  late final TextEditingController _textEditingController;
-
-  final List<String> newsList = [
-    'a',
-    'bb',
-    'ccc',
-    'dddd',
-  ];
-
-  @override
-  void initState() {
-    _textEditingController = TextEditingController();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // appBar: _appbar(context),
-      body: _body(context),
+    return DefaultTabController(
+      length: 3, // Number of tabs
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: _buildAppBar(context),
+        body: _buildBody(context),
+      ),
     );
   }
 
-  _appbar(BuildContext context) {
-    return Container(
-      width: MediaQuery.sizeOf(context).width,
-      height: MediaQuery.sizeOf(context).height * 0.25,
-      decoration: const BoxDecoration(
-        color: Color(0xff327B90),
-        borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+  PreferredSizeWidget _buildAppBar(BuildContext context) {
+    return AppBar(
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back, color: Colors.white),
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            // Text
-            Row(
-              children: [
-                BackButton(
-                  color: const Color(0xffFFFFFF),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                Text(
-                  'NEWS',
-                  style: GoogleFonts.raleway(
-                    color: const Color(0xffFFFFFF),
-                    fontSize: 28,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ],
-            ),
-
-            // Button
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.only(
-                      top: 30,
-                      bottom: 30,
-                      right: 20,
-                    ),
-                    child: CustomSearchTab(
-                      child: TextField(
-                        controller: _textEditingController,
-                        decoration: textfieldSearchDec('Search'),
-                      ),
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(
-                    Icons.filter_alt_outlined,
-                    size: 32,
-                    color: Color(0xffFFFFFF),
-                  ),
-                  onPressed: () {}, // TODO: Filter News
-                ),
-              ],
-            ),
-          ],
+      title: Text(
+        'NEWS',
+        style: GoogleFonts.raleway(
+          color: Colors.white,
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
         ),
       ),
+      backgroundColor: const Color(0xff327B90),
+      elevation: 0,
     );
   }
 
-  Widget _body(BuildContext context) {
-    return SafeArea(
+  Widget _buildBody(BuildContext context) {
+    return SingleChildScrollView(
       child: Column(
         children: [
-          _appbar(context),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: _listNews(context),
-          ),
+          _buildProfileSection(context),
+          _buildTabBarSection(context),
+          _buildTabBarViewSection(context),
         ],
       ),
     );
   }
 
-  _listNews(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: (context, index) {
-        final news = newsList[index];
-        return Padding(
+  Widget _buildProfileSection(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
           padding: const EdgeInsets.all(8.0),
-          child: NewsTile(newsTitle: news[index], detail: 'aaa',),
-        );
-      },
-      itemCount: newsList.length,
+          child: Container(
+            width: MediaQuery.sizeOf(context).width * 0.8,
+            height: MediaQuery.sizeOf(context).height * 0.18,
+            decoration: BoxDecoration(
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(
+                  'OH/CAMP/TrendedComp',
+                  style: GoogleFonts.raleway(
+                    color: const Color(0xff000000),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildTabBarSection(BuildContext context) {
+    return const TabBar(
+      labelColor: Colors.blue,
+      unselectedLabelColor: Colors.grey,
+      indicatorColor: Colors.blue,
+      tabs: [
+        Tab(text: 'Openhouse'),
+        Tab(text: 'Camp'),
+        Tab(text: 'Competition'),
+      ],
+    );
+  }
+
+  Widget _buildTabBarViewSection(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6, // Adjust height as needed
+      child: TabBarView(
+        children: [
+          _buildTabContent(context, 'Openhouse'),
+          _buildTabContent(context, 'Camp'),
+          _buildTabContent(context, 'Competition'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTabContent(BuildContext context, String tabName) {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'สายวิทย์',
+            style: GoogleFonts.raleway(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+              color: const Color(0xffA1E8AF),
+            ),
+          ),
+          const SizedBox(height: 8),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 4, // Number of topics
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Topic ${index + 1}',
+                            style: GoogleFonts.raleway(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: const Color(0xff327B90),
+                            ),
+                          ),
+                          Text(
+                            'subtopic subtopic subtopic\nsubtopic subtopic',
+                            style: GoogleFonts.raleway(
+                              fontSize: 16,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                        width: 80,
+                        height: 60,
+                        color: Colors.grey[300],
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
