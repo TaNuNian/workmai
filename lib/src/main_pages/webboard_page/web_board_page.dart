23f419f1660292e133ec04f9b1978c78b6f5d4a6
OR_TH <watcharaw.boon@gmail.com>
2024-07-14 04:05:16 +0700

    web_board_page.dart + web_board_list_page.dart 2

--- lib/src/main_pages/webboard_page/web_board_page.dart
+++ lib/src/main_pages/webboard_page/web_board_page.dart
@@ -28,59 +28,103 @@ class _WebBoardPageState extends State<WebBoardPage> {
         child: SingleChildScrollView(
           child: Column(
             children: [
-              Container(
-                height: 200, // Adjust the height as needed
-                decoration: BoxDecoration(
-                  color: const Color(0xffD9D9D9),
-                  borderRadius: BorderRadius.circular(20),
-                ),
-              ),
+              _banner(),
               const SizedBox(height: 24),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const CircleAvatar(
-                    radius: 30,
-                  ),
-                  const SizedBox(width: 12), // Space between avatar and text
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  Row(
                     children: [
-                      Text(
-                        'NAME',
-                        style: GoogleFonts.raleway(
-                          color: const Color(0xffA9D95A),
-                          fontSize: 22,
-                          fontWeight: FontWeight.w500,
-                        ),
+                      const CircleAvatar(
+                        radius: 30,
                       ),
-                      Text(
-                        'dd/mm/yy',
-                        style: GoogleFonts.raleway(
-                          color: const Color(0xff327B90),
-                          fontSize: 16,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      )
+                      const SizedBox(width: 12),
+                      // Space between avatar and text
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          _displayname(),
+                          _date(),
+                        ],
+                      ),
                     ],
                   ),
-                ],
+_commentlike(),                ],
               ),
               const SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Text(
-                  'yapyapyapyapyapyapyapyapyapyapyapyapyapyap',
-                  style: GoogleFonts.raleway(
-                    color: const Color(0xff000000),
-                    fontSize: 18,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              )
+              _textbody()
             ],
           ),
         ),
       ),
     );
+  }
+
+  Widget _banner() {
+    return Container(
+      height: 200, // Adjust the height as needed
+      decoration: BoxDecoration(
+        color: const Color(0xffD9D9D9),
+        borderRadius: BorderRadius.circular(20),
+      ),
+    );
+  }
+
+  Widget _displayname() {
+    return Text(
+      'NAME',
+      style: GoogleFonts.raleway(
+        color: const Color(0xffA9D95A),
+        fontSize: 22,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _date() {
+    return Text(
+      'dd/mm/yy',
+      style: GoogleFonts.raleway(
+        color: const Color(0xff327B90),
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _textbody() {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Text(
+        'yapyapyapyapyapyapyapyapyapyapyapyapyapyap',
+        style: GoogleFonts.raleway(
+          color: const Color(0xff000000),
+          fontSize: 18,
+          fontWeight: FontWeight.w300,
+        ),
+      ),
+    );
+  }
+
+  Widget _commentlike() {
+    return Row(
+      children: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.comment,
+            color: Colors.grey,
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.thumb_up,
+            color: Colors.grey,
+          ),
+        ),
+      ],
+    )
+    ;
   }
 }
